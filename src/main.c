e47e67fba488defd749d125e3cb8a1d1d43084d3
Tendou26 <jjbrathers3@gmail.com>
2023-10-19 13:38:18 +0700

    running in vscode check#1

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main(){
+    int number;
+    printf("Input number please : \n");
+    scanf("%d", &number);
+    printf("Your input number is %d", number);
+    return 0;
+}
