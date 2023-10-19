d01896aa1aafcc547f80ab36429a41f967538783
Tendou26 <jjbrathers3@gmail.com>
2023-10-19 15:42:59 +0700

    homepage design

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include "stdio.h"
+#include <stdlib.h>
 
 int main(){
-    int number;
-    printf("Input number please : \n");
-    scanf("%d", &number);
-    printf("Your input number is %d", number);
-    return 0;
+    char option;
+    void create(){
+        printf("create\n");
+    }
+    /* instruction : 
+        1 create
+        2 read
+        3 edit
+        4 delete
+        5 exit
+    */
+    while(option != 5){
+   scanf("%c", &option);
+        // printf("type is : %lu\n", sizeof(option));
+        switch (option){
+        case 1:
+            create();
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            // printf("Please insert 1-5 number\n");
+            printf("default\n");
+            break;
+        }
+        // scanf("%d", &option);
+    }
+    return 1;
 }
